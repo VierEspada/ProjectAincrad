1e7bb8427911cd7dbfcd5c6154e3bcf80fd29fc4
RUSK <ras9rusk@outlook.com>
2026-04-23 16:02:35 +0900

    Create cf_script.js

---
+++ cf_script.js
@@ -0,0 +1,23 @@
+fetch("cf.json")
+  .then(res => res.json())
+  .then(data => {
+    createList("shopList", data.shops);
+    createList("npcList", data.npcs);
+    createList("questList", data.quests);
+    createList("chestList", data.chests);
+  });
+
+function createList(id, items) {
+  const list = document.getElementById(id);
+
+  items.forEach(item => {
+    const li = document.createElement("li");
+    li.textContent = item.name;
+
+    li.onclick = () => {
+      window.location.href = item.link;
+    };
+
+    list.appendChild(li);
+  });
+}
