53a2eac0ca022e2591bc0481429488b097639dbc
RUSK <ras9rusk@outlook.com>
2026-05-01 13:45:47 +0900

    Create tolbana_script.js

---
+++ tolbana_script.js
@@ -0,0 +1,25 @@
+fetch("tolbana.json")
+  .then(res => res.json())
+  .then(data => {
+    createList("wshopList", data.wshop);
+    createList("ishopList", data.ishop);
+    createList("zakkaList", data.zakka);
+    createList("questList", data.quests);
+    createList("chestList", data.chests);
+    createList("ryokanList", data.ryokan);
+  });
+
+function createList(id, items) {
+  const list = document.getElementById(id);
+
+  items.forEach(item => {
+    const li = document.createElement("li");
+    li.textContent = item.name;
+
+    li.onclick = () => {
+      window.location.href = item.link;
+    };
+
+    list.appendChild(li);
+  });
+}
