83ec8e49e891488fafaa076c95580dfcbbe3719d
RUSK <ras9rusk@outlook.com>
2026-05-01 12:42:47 +0900

    Create horunkai2.js

---
+++ horunkai2.js
@@ -0,0 +1,15 @@
+{
+  "name": "Ron's item Shop",
+  "image": "horunkai2.png",
+  "description": "ランタンを買える。",
+
+  "items": [
+    {
+      "image": "Lantern.png",
+      "name": "Lantern",
+      "buy": 2500,
+      "sell": 1250,
+      "link": "Lantern.html"
+    }
+  ]
+}
